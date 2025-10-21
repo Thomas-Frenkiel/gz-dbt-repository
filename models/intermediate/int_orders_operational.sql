c45f8e8cd73c833dabb7b2b28cf187afa9522ead
frenkielthomas@gmail.com <frenkielthomas@gmail.com>
2025-10-21 13:53:38 +0000

    update order by

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,10 @@
+SELECT 
+orders_id,
+date_date,
+ROUND(SUM (margin + shipping_fee - logCost - ship_cost)) AS operational_margin
+
+FROM {{ ref('int_orders_margin') }}
+LEFT JOIN {{ ref('stg_ship') }}
+USING (orders_id)
+GROUP BY orders_id, date_date
+ORDER BY date_date DESC, orders_id 

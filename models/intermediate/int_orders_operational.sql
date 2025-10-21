b67488a8d06856b7050d5a83d7ae44ce88bed351
frenkielthomas@gmail.com <frenkielthomas@gmail.com>
2025-10-21 14:29:23 +0000

    log_cost column

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,11 +1,13 @@
 SELECT 
 orders_id,
 date_date,
-ROUND(SUM (margin + shipping_fee - logCost - ship_cost)) AS operational_margin
+ROUND(SUM (margin + shipping_fee - logCost - ship_cost)) AS operational_margin,
 SUM(revenue) AS revenue,
-SUM(shipping_fee) AS shipping_fee
+SUM(shipping_fee) AS shipping_fee,
+SUM(purchase_cost) AS purchase_cost,
+SUM(logCost) AS log_cost,
 FROM {{ ref('int_orders_margin') }}
-LEFT JOIN {{ ref('stg_ship') }}
+INNER JOIN {{ ref('stg_ship') }}
 USING (orders_id)
 GROUP BY orders_id, date_date
 ORDER BY date_date DESC, orders_id
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -6,8 +6,8 @@ SUM(revenue),
 SUM(operational_margin) AS operational_margin,
 SUM(purchase_cost) AS purchase_cost,
 SUM(shipping_fee) AS shipping_fee,
-SUM(logCost) AS log_cost,
-COUNT(products_id)
+SUM(log_cost) AS log_cost,
+COUNT(products_id) AS product
 
 FROM {{ ref('int_orders_operational') }}
 GROUP BY date_date, orders_id

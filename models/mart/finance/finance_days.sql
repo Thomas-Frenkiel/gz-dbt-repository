d2fcb009293d0cf7b2cb234619b2f41ddf0288e1
Thomas FRENKIEL <frenkielthomas@gmail.com>
2025-10-22 13:56:59 +0000

    Correction

--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -9,7 +9,8 @@ ROUND(SUM(operational_margin)) AS operational_margin,
 ROUND(SUM(purchase_cost)) AS purchase_cost,
 ROUND(SUM(shipping_fee)) AS shipping_fee,
 ROUND(SUM(log_cost)) AS log_cost,
-SUM(quantity) AS quantity
+SUM(quantity) AS quantity,
+SUM(ship_cost) AS ship_cost
 
 FROM {{ ref('int_orders_operational') }}
 GROUP BY date_date
